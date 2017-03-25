8660b8a52354c84669e05a65de8763012266a09a
lealex262 <lealex262@gmail.com>
2017-03-25 15:24:52 -0400

    Setup app structure

--- StudyWaves/app/src/main/java/hackemory/studywaves/controller/WelcomePage.java
+++ StudyWaves/app/src/main/java/hackemory/studywaves/controller/WelcomePage.java
@@ -1,4 +1,4 @@
-package hackemory.studywaves;
+package hackemory.studywaves.controller;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import hackemory.studywaves.R;
 
 public class WelcomePage extends AppCompatActivity {
 
